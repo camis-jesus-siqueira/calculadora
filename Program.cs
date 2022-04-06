c7ce5f8b409cb5f75437cabc1174805729f4235d
camis-jesus-siqueira <102191322+camis-jesus-siqueira@users.noreply.github.com>
2022-04-06 04:53:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CalculadoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome");
+            string nome = Console.ReadLine();
+
+             Console.WriteLine("Entre com sua altura. Ex: 1,65");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+             Console.WriteLine("Entre com o seu peso em kg. Ex:67,7");
+             float peso = Convert.ToSingle(Console.ReadLine());
+
+             double imc = peso / (altura* altura);
+             
+             Console.WriteLine("{0} sua altura é {1} e o seu peso {2}", nome, altura, peso);
+             Console.WriteLine("Seu IMC é {0}", imc);
+
+             if(imc < 17){
+                Console.WriteLine("Você está bem abaixo do peso");
+            }else if(imc > 17 && imc < 18.5){
+                Console.WriteLine("Você está abaixo do peso");
+            }else if(imc > 18.5 && imc < 25){
+                Console.WriteLine("Você está com o peso ideal");
+            }else if(imc > 25 && imc < 30){
+                Console.WriteLine("Você está acima do peso");
+            }else if(imc > 30 && imc < 35){
+                Console.WriteLine("Você está com obesidade tipo I");
+            }else if(imc > 35 && imc < 40){
+                Console.WriteLine("Você está com  Obesidade severa tipo II");
+            }else  if (imc > 40){
+                Console.WriteLine(" voce esta com Obesidade mórbida tipo III");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+## Calculadora IMC
+
+Aplicaçao console para o calculo do IMC.
+## Funcionalidades
+
+- Calcula e apresenta o IMC 
+- Exibe a classificaçao do grau de obesidade de acordo com a tabela
+## Tecnologias ultilizadas
+ - dotnet
+ ## Como executar 
+ 
+ Realize o clone do repositorio e execute o comando:
+
+ ```
+ dotnet run
+ ```
